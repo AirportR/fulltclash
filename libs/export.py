cfee54eed9ffbed8ac54b1488130f7ea740212a3
AirportR <airportroster@gmail.com>
2023-02-19 15:04:17 +0800

    :memo: 3.5.1: Update version number

--- libs/export.py
+++ libs/export.py
@@ -17,7 +17,7 @@ from libs.emoji_custom import TwitterPediaSource
 2、何为基础数据？
     基础数据决定了生成图片的高度（Height），它是列表，列表里面的数据一般是一组节点名，即有多少个节点就对应了info键值中的长度。
 """
-__version__ = "3.5.0"  # 版本号
+__version__ = "3.5.1-dev"  # 版本号
 custom_source = TwitterPediaSource  # 自定义emoji风格 TwitterPediaSource
 
 
