46e64f9a89ba73e3106cdf691b43c496a0b35f36
mlmmlm <103432996+mlmmlm@users.noreply.github.com>
2023-03-27 22:17:14 +0800

    Update speedtest.py

--- libs/speedtest.py
+++ libs/speedtest.py
@@ -192,19 +192,27 @@ async def batch_speed(message: Message, nodename: list, proxygroup='auto'):
             await message.delete(revoke=False)
             break
         progress += 1
-        cal = progress / nodenum * 100
-        p_text = "%.2f" % cal
+        cal = progress / nodenum
+        bar_length = 50
+        num_eq = int(cal * bar_length)
+        num_space = bar_length - num_eq
+        p_text = "%.2f" % (cal * 100)
         IKM = InlineKeyboardMarkup(
             [
                 [InlineKeyboardButton("👋中止测速", callback_data='stop')],
             ]
         )
         # 判断进度条，每隔10%发送一次反馈，有效防止洪水等待(FloodWait)
-        if cal >= sending_time:
+        if cal * 100 >= sending_time:
+            eq_ratio = int(cal * 100 / 2)
+            eq = '=' * (1 + num_eq * eq_ratio // 100)
+            space = ' ' * num_space
+            bar = eq + space
+            bar_with_frame = '[{}]'.format(bar)
             sending_time += 10
             try:
                 # 实时反馈进度
-                await message.edit_text(speedtext+"\n\n" +
+                await message.edit_text(speedtext + '\n' + '\n' + bar_with_frame + "\n\n"
                                         "当前进度:\n" + p_text +
                                         "%     [" + str(progress) + "/" + str(nodenum) + "]", reply_markup=IKM)
             except RPCError as r:
@@ -230,13 +238,18 @@ async def batch_udp(message, nodename: list, proxygroup='auto'):
             info[test_items[i]].append(res2[i])
 
         progress += 1
-        cal = progress / nodenum * 100
-        p_text = "%.2f" % cal
+        cal = progress / nodenum
+        bar_length = 26
+        num_eq = int(cal * bar_length)
+        num_space = bar_length - num_eq
+        p_text = "%.2f" % (cal * 100)
         # 判断进度条，每隔10%发送一次反馈，有效防止洪水等待(FloodWait)
-        if cal >= sending_time:
+        if cal * 100 >= sending_time:
             sending_time += 10
+            bar = '=' * num_eq + ' ' * num_space
+            bar_with_frame = '[{}]'.format(bar)
             try:
-                await message.edit_text(udptext+"\n\n" +
+                await message.edit_text(udptext+"\n\n" + bar_with_frame + "\n\n" +
                                         "当前进度:\n" + p_text +
                                         "%     [" + str(progress) + "/" + str(nodenum) + "]")  # 实时反馈进度
             except RPCError as r:
