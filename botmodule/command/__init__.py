8d742d841e70b2bdaf633edbf17e1a0cebc24ef7
AirportR <airportroster@gmail.com>
2023-04-14 23:41:45 +0800

    :art: Add a custom command trigger

--- botmodule/command/__init__.py
+++ botmodule/command/__init__.py
@@ -1,9 +1,10 @@
 from botmodule.command.test import test
 from pyrogram.types import Message
-from utils import message_delete_queue
+# from utils import message_delete_queue
 
 
-async def common_command(_, message: Message):
-    backmsg = await message.reply("如果你看到这条消息，说明你的权限回调工作正常。\n另外，此函数作为最后命中的组别，请勿向用户直接暴露。")
-    message_delete_queue.put_nowait((backmsg.chat.id, backmsg.id, 10))
+async def common_command(_, __: Message):
+    pass
+    # backmsg = await message.reply("如果你看到这条消息，说明你的权限回调工作正常。\n另外，此函数作为最后命中的组别，请勿向用户直接暴露。")
+    # message_delete_queue.put_nowait((backmsg.chat.id, backmsg.id, 10))
 
