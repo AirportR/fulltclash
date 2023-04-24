803a02a7825867bc01d449bc38415540fe65f5c0
AirportR <104753221+AirportR@users.noreply.github.com>
2023-04-24 21:03:21 +0800

    Update leave.py

--- botmodule/command/leave.py
+++ botmodule/command/leave.py
@@ -1,40 +1,47 @@
-import pyrogram.types
 from loguru import logger
 from pyrogram import Client
+from pyrogram.types import Message
 from pyrogram.errors import RPCError
 from botmodule.init_bot import admin, config
+from utils.check import get_telegram_id_from_message as get_id
+from utils import message_delete_queue as mdq
 
 
-async def leavechat(client: Client, message: pyrogram.types.Message):
+async def leavechat(client: Client, message: Message):
     try:
         if config.config.get('anti-group', False):
-            for user in message.new_chat_members :
+            for user in message.new_chat_members:
                 if str(user.is_self) == "True":
-                    if not int(message.from_user.id) in admin:
-                        await message.reply("❌ 机器人已启动防拉群模式，请联系超管拉群")
+                    if int(message.from_user.id) not in admin:
+                        await message.reply("❌ 机器人已启动防拉群模式，请联系管理员拉群")
                         await client.leave_chat(message.chat.id)
         return
     except RPCError as r:
         logger.error(str(r))
+    except Exception as e:
+        logger.error(str(e))
 
-async def set_anti_group(client: Client, message: pyrogram.types.Message):
-    try:
-        if int(message.from_user.id) not in admin and str(
-                message.from_user.username) not in admin:  # 如果不在USER_TARGET名单是不会有权限的
-            await message.reply("⚠️您不是bot的管理员，无法使用该命令")
-            return
-    except AttributeError:
-        if int(message.sender_chat.id) not in admin:  # 如果不在USER_TARGET名单是不会有权限的
-            await message.reply("⚠️您不是bot的管理员，无法使用该命令")
-            return
+
+async def set_anti_group(_: Client, message: Message):
+    ID = get_id(message)
+    if ID not in admin:
+        backmsg = await message.reply("⚠️您不是bot的管理员，无法使用该命令")
+        mdq.put_nowait((backmsg.chat.id, backmsg.id, 10))
+        return
     try:
         if config.config.get('anti-group', True):
             config.yaml['anti-group'] = False
+            config.reload()
             logger.info("关闭了防拉群")
-            await message.reply("关闭了防拉群")
+            backmsg = await message.reply("关闭了防拉群")
+            mdq.put_nowait((backmsg.chat.id, backmsg.id, 10))
         else:
             config.yaml['anti-group'] = True
+            config.reload()
             logger.info("开启了防拉群")
-            await message.reply("开启了防拉群")
+            backmsg = await message.reply("开启了防拉群")
+            mdq.put_nowait((backmsg.chat.id, backmsg.id, 10))
     except RPCError as r:
-        print(r)
+        logger.error(str(r))
+    except Exception as e:
+        logger.error(str(e))
