db648c4e903807d8ee66f1ac759f682da2080f84
AirportR <airportroster@gmail.com>
2023-05-09 12:21:54 +0800

    :art: Fixed openai.py

--- addons/unlockTest/openai.py
+++ addons/unlockTest/openai.py
@@ -2,7 +2,6 @@ import asyncio
 import aiohttp
 from aiohttp import ClientConnectorError
 from loguru import logger
-from pyrogram.types import InlineKeyboardButton
 
 # collector section
 SUPPORT_REGION = ['AL', 'DZ', 'AD', 'AO', 'AG', 'AR', 'AM', 'AU', 'AT', 'AZ', 'BS', 'BD', 'BB', 'BE', 'BZ', 'BJ', 'BT',
@@ -11,7 +10,7 @@ SUPPORT_REGION = ['AL', 'DZ', 'AD', 'AO', 'AG', 'AR', 'AM', 'AU', 'AT', 'AZ', 'B
                   'SV',
                   'EE', 'FJ', 'FI', 'FR', 'GA', 'GM', 'GE', 'DE', 'GH', 'GR', 'GD', 'GT', 'GN', 'GW', 'GY', 'HT', 'HN',
                   'HU',
-                  'IS', 'IN', 'ID', 'IQ', 'IE', 'IL', 'IT', 'JM', 'JP', 'JO', 'KZ', 'KE', 'KI', 'KW', 'KG', 'LV', 'LB',
+                  'IS', 'IN', 'ID', 'IQ', 'IE', 'IL', 'JM', 'JP', 'JO', 'KZ', 'KE', 'KI', 'KW', 'KG', 'LV', 'LB',
                   'LS',
                   'LR', 'LI', 'LT', 'LU', 'MG', 'MW', 'MY', 'MV', 'ML', 'MT', 'MH', 'MR', 'MU', 'MX', 'MC', 'MN', 'ME',
                   'MA',
@@ -43,10 +42,15 @@ async def fetch_openai(Collector, session: aiohttp.ClientSession, proxy=None, re
         async with session.get(openaiurl1, headers=_headers, proxy=proxy, timeout=10) as res:
             if res.status == 403:
                 text = await res.text()
-                index = text.find("You do not have access to chat.openai.com.")
+                index = text.find("Sorry, you have been blocked")
                 if index > 0:
                     Collector.info['OpenAI'] = "失败"
                     return
+                index2 = text.find("You do not have access to chat.openai.com.")
+                if index2 > 0:
+                    Collector.info['OpenAI'] = "失败"
+                    return
+
             else:
                 Collector.info['OpenAI'] = "未知"
         async with session.get(openaiurl2, headers=_headers, proxy=proxy, timeout=10) as res2:
@@ -54,7 +58,7 @@ async def fetch_openai(Collector, session: aiohttp.ClientSession, proxy=None, re
                 text2 = await res2.text()
                 index2 = text2.find("loc=")
                 if index2 > 0:
-                    region = text2[index2+4:index2+6]
+                    region = text2[index2 + 4:index2 + 6]
                     if region in SUPPORT_REGION:
                         Collector.info['OpenAI'] = f"解锁({region})"
                     else:
@@ -103,22 +107,8 @@ def get_openai_info(ReCleaner):
         return "N/A"
 
 
-# bot_setting_board
-
-button = InlineKeyboardButton("✅OpenAI", callback_data='✅OpenAI')
-
-if __name__ == "__main__":
-    "this is a test demo"
-    import sys
-    import os
-
-    sys.path.append(os.path.abspath(os.path.join(os.getcwd(), os.pardir, os.pardir)))
-    from utils.collector import Collector as CL, media_items
-
-    media_items.clear()
-    media_items.append("OpenAI")
-    cl = CL()
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(cl.start(proxy="http://127.0.0.1:1111"))
-    print(cl.info)
+SCRIPT = {
+    "MYNAME": "OpenAI",
+    "TASK": task,
+    "GET": get_openai_info
+}
