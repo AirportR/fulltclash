eae684b02e9eb63a19a0431ef0434cb92b16f0a9
AirportR <airportroster@gmail.com>
2023-06-01 11:55:36 +0800

    :art: MessageDeleteQueue added a function.

--- utils/cron/utils.py
+++ utils/cron/utils.py
@@ -1,5 +1,7 @@
 import asyncio
 
+from pyrogram.types import Message
+
 
 class MessageDeleteQueue(asyncio.Queue):
     """
@@ -14,6 +16,13 @@ class MessageDeleteQueue(asyncio.Queue):
         put the value as a tuple: (chat.id, msg.id, seconds)
         """
         super().put_nowait(item)
+
+    def put(self, msg: Message, second=10):
+        """
+        快速推入删除队列，仅需要传入pyrogram.types.Message 对象，推荐使用！
+        second: 设定多少秒后删除
+        """
+        super().put_nowait((msg.chat.id, msg.id, second))
 
 
 message_delete_queue = MessageDeleteQueue()  # put the value as a tuple: (chat.id, msg.id, seconds)
