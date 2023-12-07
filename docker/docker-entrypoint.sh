b9f587da2fd7924f91e4d5dba45957ad7a543265
cola <44671411+aipeach@users.noreply.github.com>
2023-12-08 02:58:13 +0800

    add alpine docker

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,4 +2,10 @@
 
 supervisord -c /etc/supervisord.conf
 
-cron -f > /dev/null 2>&1
+if [[ -f "/etc/debian_version" ]]; then
+    cron -f > /dev/null 2>&1
+fi
+
+if [[ -f "/etc/alpine-release" ]]; then
+    crond -f > /dev/null 2>&1
+fi
