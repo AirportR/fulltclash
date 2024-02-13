0662916e1a8199c84ab0e9014daaf0deb1a2888e
AirportR <AirportRoster@gmail.com>
2024-02-13 21:17:22 +0800

    :bug: 3.6.7: Fix openai.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,39 @@
+import aiohttp
+
 from utils.cron import *
+from typing import Callable, Any
 
 __version__ = "3.6.7"  # 项目版本号
-__all__ = ["cron_delete_message", "cron_edit_message", "message_delete_queue", "message_edit_queue", "__version__"]
+__all__ = ["cron_delete_message", "cron_edit_message", "message_delete_queue", "message_edit_queue", "__version__",
+           "retry"]
+
+
+def default_breakfunc(ret_val: bool) -> bool:
+    return True if isinstance(ret_val, bool) and ret_val else False
+
+
+def retry(count=5, break_func: Callable[[Any], bool] = None):
+    """
+    重试装饰器，网络请求可能会出错，所以需要重试
+    :param: count: 重试次数
+    :param: break_func: 触发的回调中止函数，参数为调用函数的返回值，返回值为bool，
+    """
+    if break_func is None:
+        break_func = default_breakfunc
+
+    def wrapper(func):
+        async def inner(*args, **kwargs):
+            for _ in range(count):
+                try:
+                    if asyncio.iscoroutinefunction(func):
+                        result = await func(*args, **kwargs)
+                    else:
+                        result = func(*args, **kwargs)
+                except aiohttp.ClientError:
+                    continue
+                if break_func(result):
+                    break
+
+        return inner
+
+    return wrapper
