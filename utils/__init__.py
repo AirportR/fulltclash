eaad9bbf8b8510ec127065569b537177639dbd35
koipy <aasg9414@gmail.com>
2024-03-23 22:43:20 +0800

    :art: 4.1.5: 适配retry装饰器

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,9 @@
+import asyncio
 import random
 from os import getcwd
+from typing import Callable, Any, Union, Coroutine
 
+import aiohttp
 from aiohttp import http
 
 __version__ = "4.1.5"
@@ -27,6 +30,54 @@ def generate_random_string():
 def block_aiohttp_version():
     # 随机响应头，迷惑网络探测，降低在网络中的存在感
     http.SERVER_SOFTWARE = generate_random_string()
+
+
+def default_breakfunc(ret_val: bool) -> bool:
+    return True if isinstance(ret_val, bool) and ret_val else False
+
+
+def retry(count=5, break_func: Callable[[Any], bool] = None):
+    """
+    重试装饰器，网络请求可能会出错，所以需要重试
+    :param: count: 重试次数
+    :param: break_func: 触发的回调中止函数，参数为调用函数的返回值，返回值为bool，
+    """
+    if break_func is None:
+        break_func = default_breakfunc
+
+    def wrapper(func):
+        async def inner(*args, **kwargs):
+            for _ in range(count):
+                try:
+                    if asyncio.iscoroutinefunction(func):
+                        result = await func(*args, **kwargs)
+                    else:
+                        result = func(*args, **kwargs)
+                except (aiohttp.ClientError, asyncio.exceptions.TimeoutError, ConnectionResetError, Exception):
+                    continue
+                if break_func(result):
+                    break
+
+        return inner
+
+    return wrapper
+
+
+async def script_demo(script_func: Union[Callable, Coroutine], *arg, **kwargs):
+    class FakeColl:
+        def __init__(self):
+            self.info = {}
+            self.data = self.info
+
+    fakecl = FakeColl()
+
+    session = aiohttp.ClientSession()
+    if asyncio.iscoroutine(script_func):
+        await script_func
+    else:
+        await script_func(fakecl, session, *arg, **kwargs)
+    print(fakecl.info)
+    await session.close()
 
 
 block_aiohttp_version()
